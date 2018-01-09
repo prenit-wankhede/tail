3b42c2062ad22953ff31daaa82527ef4412cacf1
prenit-wankhede <prenit.wankhede.me@gmail.com>
2018-01-09 13:55:58 +0530

    no need for user id. started a thread in application.rb to send broadcast to all users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,30 +3,5 @@ class HomeController < ApplicationController
   	@file_path = Rails.root.join('test_file.txt')
   	@last_10_lines = ApplicationHelper.tail(@file_path, 10)
   	@last_10_lines_array = @last_10_lines.split("\n")
-
-  	
-  	@user_id = params[:user_id]
-  	if(@user_id.blank?)
-  		render
-  	else
-	  	t = Thread.new(@user_id, @file_path, @last_10_lines_array) do |user_id, file_path, last_10_lines_array|
-	  		sleep(5)
-		    while true do
-		      	@new_last_10_lines = ApplicationHelper.tail(file_path, 10)
-		      	@new_last_10_lines_array = @new_last_10_lines.split("\n")
-
-		      	@new_lines_array = @new_last_10_lines_array - last_10_lines_array
-			  	if(!@new_lines_array.blank?)
-			  		ActionCable.server.broadcast("user_#{user_id}", {new_lines: @new_lines_array})
-			  		last_10_lines_array = @new_last_10_lines_array
-			  		@new_lines_array = []
-			  	end
-
-			  	sleep(5)
-		    end
-		end
-
-  	end
-
   end
 end
